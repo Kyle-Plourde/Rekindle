e9c32a3a633d8777db93bb4c4b1b4580f1013623
Kyle-plourde <32370011+Kyle-Plourde@users.noreply.github.com>
2018-03-13 18:11:10 -0500

    add social buttons backend functional

--- ReKindle.js
+++ ReKindle.js
@@ -1,19 +1,49 @@
 function init()
 {
-    localStorage.setItem('james', false);
+    localStorage.setItem('james', 'false');
 }
 
 function addjames()
 {
-    localStorage.setItem('james', true);
+    localStorage.setItem('james', 'true');
     document.location.href = "home_one_contact.html";
 }
 
-function jamesadded ()
+function jamesadded()
 {
     var james = localStorage.getItem('james');
     if (james)
     {
         document.getElementById("james").style.display = "block"
+    }
+}
+
+function addfb()
+{
+    if (localStorage.getItem('fb') === 'false'){
+        localStorage.setItem('fb', 'true');
+    }
+    else {
+        localStorage.setItem('fb', 'false');
+    }
+}
+
+function addinsta()
+{
+    if (localStorage.getItem('insta') === 'false'){
+        localStorage.setItem('insta', 'true');
+    }
+    else {
+        localStorage.setItem('insta', 'false')
+    }
+}
+
+function addsnap()
+{
+    if (localStorage.getItem('snap') === 'false'){
+        localStorage.setItem('snap', 'true');
+    }
+    else {
+        localStorage.setItem('snap', 'false');
     }
 }
